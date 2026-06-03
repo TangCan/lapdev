84324ba176daf153f71fa9af201a4c077a64d781
Richard Tang <richard.tang@grchain.io>
2026-06-03 12:22:40 +0800

    chore: 更新依赖并集成Skill系统
    - 添加js-yaml依赖用于Skill文件解析
    - 在App中集成SkillProvider
    - 更新sprint状态 - 4-1-skill-development完成

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import { ChatProvider } from './context/ChatContext';
 import { AIProvider } from './context/AIContext';
 import { AgentProvider } from './context/AgentContext';
 import { InlineCompletionProvider } from './context/InlineCompletionContext';
+import { SkillProvider } from './context/SkillContext';
 import { SettingsPage } from './pages/SettingsPage';
 import IDE from './components/IDE/IDE';
 
@@ -14,14 +15,16 @@ function App() {
       <LSPProvider>
         <ChatProvider>
           <AIProvider>
-            <AgentProvider>
-              <InlineCompletionProvider>
-                <Routes>
-                  <Route path="/" element={<IDE />} />
-                  <Route path="/settings" element={<SettingsPage />} />
-                </Routes>
-              </InlineCompletionProvider>
-            </AgentProvider>
+            <SkillProvider>
+              <AgentProvider>
+                <InlineCompletionProvider>
+                  <Routes>
+                    <Route path="/" element={<IDE />} />
+                    <Route path="/settings" element={<SettingsPage />} />
+                  </Routes>
+                </InlineCompletionProvider>
+              </AgentProvider>
+            </SkillProvider>
           </AIProvider>
         </ChatProvider>
       </LSPProvider>
